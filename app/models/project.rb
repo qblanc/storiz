ba36cebcb54d8d4d62d5b5fff630736dd51eeea3
Paul Portier <paulportier@gmail.com>
2019-11-25 16:37:38 +0100

    projects_controller initialized

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :characters
   has_many :scenes
+
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :category, presence: true, inclusion: { in: %w(Fantastic Thriller Comedy) } # !!! a implémenter les bitches
 end
