7ae207e6750ff73f7fbb3189a89896cc37681af7
Corentin Huillard <corentin.huillard@gmail.com>
2019-11-25 17:21:25 +0100

    bug routes, fixed typos

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,4 +1,7 @@
 class Scene < ApplicationRecord
   belongs_to :project
-  has_many :charscenes
+  has_many :charascenes
+  has_many :characters, through: :charascenes
+
+  validates :number, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :projects, only: [:new, :create, :edit, :update, :show, :index, :destroy] do
-  #   resources :scenes, [:new, :create, :edit, :update, :show, :index, :destroy]
-  #   resources :characters, only: [:new, :create, :edit, :update, :show, :index, :destroy]
-  # end
+  resources :scenes, only: [:edit, :update, :destroy]
+
+  resources :characters, only: [:edit, :update, :destroy]
+
+  resources :projects, only: [:new, :create, :edit, :update, :show, :index, :destroy] do
+    resources :scenes, [:new, :create, :show, :index]
+    resources :characters, only: [:new, :create, :show, :index]
+  end
 end
