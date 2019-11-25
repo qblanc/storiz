b03eacaa96175a4fd0d4ea9f14b5c73def01c796
Paul Portier <paulportier@gmail.com>
2019-11-25 15:42:48 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects, only: [:new, :create, :edit, :update, :show, :index, :destroy] do
+    resources :scenes, [:new, :create, :edit, :update, :show, :index, :destroy]
+    resources :characters, only: [:new, :create, :edit, :update, :show, :index, :destroy]
+  end
 end
