6964ce5adcb81e2287f7ed0c3f24473fed4341eb
Corentin Huillard <corentin.huillard@gmail.com>
2019-11-25 17:25:08 +0100

    denested some routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :scenes, only: [:edit, :update, :destroy]
+  resources :scenes, only: [:edit, :update, :show, :index, :destroy]
 
-  resources :characters, only: [:edit, :update, :destroy]
+  resources :characters, only: [:edit, :update, :show, :index, :destroy]
 
   resources :projects, only: [:new, :create, :edit, :update, :show, :index, :destroy] do
-    resources :scenes, [:new, :create, :show, :index]
-    resources :characters, only: [:new, :create, :show, :index]
+    resources :scenes, [:new, :create]
+    resources :characters, only: [:new, :create]
   end
 end
