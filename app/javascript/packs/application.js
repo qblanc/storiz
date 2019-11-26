6270c3c2aa0abe154eea0b52d8f8868b4ef4b930
Quentin Blanc <qblanc@hotmail.com>
2019-11-26 19:00:23 +0100

    text autosave

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import "bootstrap";
 import { initDisplayCharShowOnClick } from '../pages/bible';
+import { TextSave } from '../components/text_save';
 import Rails from '@rails/ujs';
 Rails.start();
 
 initDisplayCharShowOnClick();
+TextSave();
 
 require("trix")
 require("@rails/actiontext")
