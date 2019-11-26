6249cff024cf19936ff22d0fc08449fe396f17ee
Quentin Blanc <qblanc@hotmail.com>
2019-11-26 12:17:39 +0100

    nix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def show
-    @projects = Project.all
+    @scenes = @project.scenes.all
   end
 
   def index
