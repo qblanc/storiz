7284d4b23bdfc2760b981551c53a486b0740f847
Corentin Huillard <corentin.huillard@gmail.com>
2019-11-26 19:23:35 +0100

    added events

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,5 +2,6 @@ class Character < ApplicationRecord
   belongs_to :project
   has_many :charascenes
   has_many :scenes, through: :charascenes
+  has_many :character_events, dependent: :destroy
   validates_with RecordNotEmptyValidator
 end
