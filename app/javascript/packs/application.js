b70ba61ff8f3d76769e6755a5a6239a57be5c896
Quentin Blanc <qblanc@hotmail.com>
2019-11-27 12:09:08 +0100

    forgot to branch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,10 @@ import "bootstrap";
 import { initDisplayCharShowOnClick } from '../pages/bible';
 import { TextSave } from '../components/text_save';
 import Rails from '@rails/ujs';
+require("trix")
+require("@rails/actiontext")
 Rails.start();
 
 initDisplayCharShowOnClick();
 TextSave();
 
-require("trix")
-require("@rails/actiontext")
