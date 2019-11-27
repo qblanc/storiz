eca52ba4d680cda6c84b7d0c6cd654080809a4af
Quentin Blanc <qblanc@hotmail.com>
2019-11-27 17:06:06 +0100

    forgot to branch again

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,13 @@
 import "bootstrap";
 import { initDisplayCharShowOnClick } from '../pages/bible';
 import { TextSave } from '../components/text_save';
+import { StickyToolbar } from '../components/sticky_toolbar'
 import Rails from '@rails/ujs';
 Rails.start();
 
 initDisplayCharShowOnClick();
 TextSave();
+StickyToolbar();
 
 require("trix")
 require("@rails/actiontext")
