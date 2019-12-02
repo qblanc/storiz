db60601725828ada7d34ace1339c8d8bd57522c5
Paul Portier <paulportier@gmail.com>
2019-12-02 14:42:16 +0100

    bible scene show fonctionnelle

--- app/javascript/components/scene_bible.js
+++ app/javascript/components/scene_bible.js
@@ -4,37 +4,51 @@ const showDetailsOnClickSceneBible = () => {
 
     setTimeout(function(){
 
-      if (document.querySelectorAll('.search-scene-results').length > 0) {
+      var searchSceneResults = document.querySelectorAll('.search-scene-results');
 
-        var searchSceneResults = document.querySelectorAll('.search-scene-results');
+      searchSceneResults.forEach((result) => {
 
-        searchSceneResults.forEach((result) => {
+        result.addEventListener('click', (event) => {
 
-          result.addEventListener('click', (event) => {
+          // Show Char Details
+          var searchSceneResultsGroup = document.querySelector('.result-list-group');
+          var searchSceneResultsHeight = searchSceneResultsGroup.offsetHeight;
 
-            var searchSceneResultsGroup = document.querySelector('.result-list-group');
-            var searchSceneResultsHeight = searchSceneResultsGroup.offsetHeight;
+          const charIdScene = event.currentTarget.dataset.searchlist;
+          const charDetailsScene = document.querySelector(`.search-scene-details[data-searchdetails="${charIdScene}"]`);
 
-            console.log(searchSceneResultsHeight);
+          charDetailsScene.style.display = 'block';
 
-            const charIdScene = event.currentTarget.dataset.searchlist;
-            const charDetailsScene = document.querySelector(`.search-scene-details[data-searchdetails="${charIdScene}"]`);
+          setTimeout(function(){
+            charDetailsScene.classList.add("slideY");
+            charDetailsScene.style.top = `-${searchSceneResultsHeight}px`;
+          }, 150);
 
-            charDetailsScene.style.display = 'block';
+          // Hide Char Details
+          const buttons = document.querySelectorAll(".hide-details-scene-bible");
 
-            setTimeout(function(){
-              charDetailsScene.classList.add("slideY");
-              charDetailsScene.style.top = `-${searchSceneResultsHeight}px`;
-            }, 100);
+          buttons.forEach((button) => {
+            var charHideId = button.dataset.hidebtn;
+            const sceneToHide = document.querySelector(`.search-scene-details[data-searchdetails="${charHideId}"]`);
 
-          });
+            button.addEventListener('click', (event) => {
+              sceneToHide.classList.remove("slideY");
+              setTimeout(function(){
+                sceneToHide.style.display = 'none';
+              }, 700);
+            });
+          })
         });
-      }
-
-    }, 100);
-
+      });
+    }, 150);
   })
+};
 
+const displayModalSceneBible = () => {
+  const addEventButtons = document.querySelectorAll('.btn-addevent-scene');
+
+    const charModalId = addEventButton.data.target
+    console.log(charModalId)
 };
 
 
