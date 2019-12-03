16e7ea3c0b32723bfd2100ec13ffade03d85b6c5
Corentin Huillard <corentin.huillard@gmail.com>
2019-12-03 17:26:08 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,54 +29,63 @@ project1.save!
 
 character1 = Character.new(first_name: "Larry", last_name: "Dimmick", birthdate: Time.now, genre: "Male", description: "Mr. White. Originally from Milwaukee, Minnesota, he's brought on Joe Cabot's current jewelry heist through prior association. Yet, he has reserves about doing it, because he feels he's jinxed. The last job he worked on, almost got him pinched by the cops. Dimmick becomes a sort of surrogate father to young recruit Mr. Orange as they plan the job over a few days. The two become so close that he lets detials of his personal life slip like his hometown and his specialty as a criminal.")
 character1.project = project1
-file1 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382305/Reservoir%20Dogs%20Characters/Mr._Blue_vrs8cd.jpg')
-character1.photo.attach(io: file1, filename: "White.jpg", content_type: 'image/jpg')
+file1 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382304/Reservoir%20Dogs%20Characters/Mr._White_mb4knr.jpg')
+character1.photo.attach(io: file1, filename: "Larry.jpg", content_type: 'image/jpg')
 character1.save!
 
-# character2 = Character.new(first_name: "Freddy", last_name: "Newandyke", birthdate: Time.now, genre: "Male", description: "Mr. Orange. He is an undercover police officer who is assigned to infilitrate Joe Cabot's latest jewelry heist. In the midst of this heist is a ragtag team of ex-felons including Mr. White (revealed to really be Larry Dimmick), whom they also have a sheet on. Conflict of interests arise when Mr. Orange has a father-son relationship with Mr. White. Mr. Orange is the alias of an undercover police officer named Freddy Newandyke who is assigned to infilitrate Joe Cabot's latest jewelry heist. In the midst of this heist is a ragtag team of ex-felons including Mr. White (revealed to really be Larry Dimmick), whom they also have a sheet on. Conflict of interests arise when Mr. Orange has a father-son relationship with Mr. White.")
-# character2.project = project1
-# character2.photo.key = "Mr._Orange_wvehin"
-# character2.save!
+character2 = Character.new(first_name: "Freddy", last_name: "Newandyke", birthdate: Time.now, genre: "Male", description: "Mr. Orange. He is an undercover police officer who is assigned to infilitrate Joe Cabot's latest jewelry heist. In the midst of this heist is a ragtag team of ex-felons including Mr. White (revealed to really be Larry Dimmick), whom they also have a sheet on. Conflict of interests arise when Mr. Orange has a father-son relationship with Mr. White. Mr. Orange is the alias of an undercover police officer named Freddy Newandyke who is assigned to infilitrate Joe Cabot's latest jewelry heist. In the midst of this heist is a ragtag team of ex-felons including Mr. White (revealed to really be Larry Dimmick), whom they also have a sheet on. Conflict of interests arise when Mr. Orange has a father-son relationship with Mr. White.")
+character2.project = project1
+file2 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382304/Reservoir%20Dogs%20Characters/Mr._Orange_wvehin.jpg')
+character1.photo.attach(io: file2, filename: "Freddy.jpg", content_type: 'image/jpg')
+character2.save!
 
-# character3 = Character.new(first_name: "Vic", last_name: "Vega", birthdate: Time.now, genre: "Male", description: "Mr. Blonde. He's a recently paroled career criminal hired by a gangster named Joe Cabot to perform a jewelry heist with a team of professional thieves. During the robbery, he makes good on a threat to massacre the entire store after the alarm had been tripped, forcing the team to flee to the designated safehouse. He takes police officer Marvin Nash hostage as he escapes. At the safehouse, he gets into a confrontation with fellow robbers Mr. White and Mr. Pink over whether or not to help their compatriot Mr. Orange, who has been badly wounded.")
-# character3.project = project1
-# character3.photo.key = "Mr._Blonde_gnvgnj"
-# character3.save!
+character3 = Character.new(first_name: "Vic", last_name: "Vega", birthdate: Time.now, genre: "Male", description: "Mr. Blonde. He's a recently paroled career criminal hired by a gangster named Joe Cabot to perform a jewelry heist with a team of professional thieves. During the robbery, he makes good on a threat to massacre the entire store after the alarm had been tripped, forcing the team to flee to the designated safehouse. He takes police officer Marvin Nash hostage as he escapes. At the safehouse, he gets into a confrontation with fellow robbers Mr. White and Mr. Pink over whether or not to help their compatriot Mr. Orange, who has been badly wounded.")
+character3.project = project1
+file3 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382304/Reservoir%20Dogs%20Characters/Mr._Blonde_gnvgnj.jpg')
+character1.photo.attach(io: file3, filename: "Vic.jpg", content_type: 'image/jpg')
+character3.save!
 
-# character4 = Character.new(first_name: "Mr.", last_name: "Pink", birthdate: Time.now, genre: "Male", description: "He has known Joe Cabot as a youth and believed that Joe had something to do with the set-up. He recalls one time when he wanted to refuse to do a job but didn't because of the money. He quit smoking, but restarted it during the film. When given his name 'Mr. Pink', he objects and says how it sounds like 'Mr. Pussy', and requests to have his name changed to something less weak like 'Mr. Purple', or to trade names with Mr. White.")
-# character4.project = project1
-# character4.photo.key = "Mr._Pink_sabdkv"
-# character4.save!
+character4 = Character.new(first_name: "Mr.", last_name: "Pink", birthdate: Time.now, genre: "Male", description: "He has known Joe Cabot as a youth and believed that Joe had something to do with the set-up. He recalls one time when he wanted to refuse to do a job but didn't because of the money. He quit smoking, but restarted it during the film. When given his name 'Mr. Pink', he objects and says how it sounds like 'Mr. Pussy', and requests to have his name changed to something less weak like 'Mr. Purple', or to trade names with Mr. White.")
+character4.project = project1
+file4 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382304/Reservoir%20Dogs%20Characters/Mr._Pink_sabdkv.jpg')
+character1.photo.attach(io: file4, filename: "Pink.jpg", content_type: 'image/jpg')
+character4.save!
 
-# character5 = Character.new(first_name: "Eddie", last_name: "Cabot", birthdate: Time.now, genre: "Male", description: "Nice Guy Eddie Cabot is the son of crime boss Joe Cabot who plan a heist on a jewellery store with 6 other men. Out of the men Eddie and his family are friendly with are Mr. White and Mr. Blonde, the latter whom Eddie appears to have a close friendship with. After recruiting and giving the gang of men their color coded nicknames, they head off for breakfast before the heist. However the heist goes wrong when the cops show up early and Mr. Blonde goes crazy and shoots dead the hostages.")
-# character5.project = project1
-# character5.photo.key = "Eddit_Cabot_svvi28"
-# character5.save!
+character5 = Character.new(first_name: "Eddie", last_name: "Cabot", birthdate: Time.now, genre: "Male", description: "Nice Guy Eddie Cabot is the son of crime boss Joe Cabot who plan a heist on a jewellery store with 6 other men. Out of the men Eddie and his family are friendly with are Mr. White and Mr. Blonde, the latter whom Eddie appears to have a close friendship with. After recruiting and giving the gang of men their color coded nicknames, they head off for breakfast before the heist. However the heist goes wrong when the cops show up early and Mr. Blonde goes crazy and shoots dead the hostages.")
+character5.project = project1
+file5 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382305/Reservoir%20Dogs%20Characters/Eddit_Cabot_svvi28.jpg')
+character1.photo.attach(io: file5, filename: "Eddie.jpg", content_type: 'image/jpg')
+character5.save!
 
-# character6 = Character.new(first_name: "Joe", last_name: "Cabot", birthdate: Time.now, genre: "Male", description: "Joe Cabot is a criminal mastermind and crime boss that specializes in armed robberies, hijackings, and moving and selling stolen property. Cabot puts together jobs, along with his son 'Nice Guy' Eddie Cabot, by recruiting different career criminals and planning out the details of each robbery. Cabot serves as the main antagonist of the movie.")
-# character6.project = project1
-# character6.photo.key = "Joe_Cabot_qgbxnp"
-# character6.save!
+character6 = Character.new(first_name: "Joe", last_name: "Cabot", birthdate: Time.now, genre: "Male", description: "Joe Cabot is a criminal mastermind and crime boss that specializes in armed robberies, hijackings, and moving and selling stolen property. Cabot puts together jobs, along with his son 'Nice Guy' Eddie Cabot, by recruiting different career criminals and planning out the details of each robbery. Cabot serves as the main antagonist of the movie.")
+character6.project = project1
+file6 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382304/Reservoir%20Dogs%20Characters/Joe_Cabot_qgbxnp.jpg')
+character1.photo.attach(io: file6, filename: "Cabot.jpg", content_type: 'image/jpg')
+character6.save!
 
-# character7 = Character.new(first_name: "Mr.", last_name: "Brown", birthdate: Time.now, genre: "Male", description: "Mr. Brown got shot in the head by a police officer, which blinds him but does not immediately kill him. Mr. Brown, Mr. White, and Mr. Orange get into a car to escape the area, but they crash because Mr. Brown was driving and unable to see. A police car arrives at the scene, and as Mr. White shoots at the police officers Mr. Orange notices that Mr. Brown dies from his injuries.")
-# character7.project = project1
-# character7.photo.key = "Mr._Brown_uzqnt9"
-# character7.save!
+character7 = Character.new(first_name: "Mr.", last_name: "Brown", birthdate: Time.now, genre: "Male", description: "Mr. Brown got shot in the head by a police officer, which blinds him but does not immediately kill him. Mr. Brown, Mr. White, and Mr. Orange get into a car to escape the area, but they crash because Mr. Brown was driving and unable to see. A police car arrives at the scene, and as Mr. White shoots at the police officers Mr. Orange notices that Mr. Brown dies from his injuries.")
+character7.project = project1
+file7 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382304/Reservoir%20Dogs%20Characters/Mr._Brown_uzqnt9.jpg')
+character1.photo.attach(io: file7, filename: "Brown.jpg", content_type: 'image/jpg')
+character7.save!
 
-# character8 = Character.new(first_name: "Marvin", last_name: "Nash", birthdate: Time.now, genre: "Male", description: "gets captured by Mr. Blonde during the robbery, and is put into the trunk of his car. Mr. White and Mr. Pink laughs when Mr. Blonde opens the trunk to see him there. He was thought to be used for questioning about the setup. He gets thrown in the warehouse and gets beaten up by them. He eventually gets tied up in a chair, and denies that he knows anything about the setup. He says how he won't tell them anything if he gets let go, only to get punched by Mr. Pink.")
-# character8.project = project1
-# character8.photo.key = "Officer_wazjnt"
-# character8.save!
+character8 = Character.new(first_name: "Marvin", last_name: "Nash", birthdate: Time.now, genre: "Male", description: "gets captured by Mr. Blonde during the robbery, and is put into the trunk of his car. Mr. White and Mr. Pink laughs when Mr. Blonde opens the trunk to see him there. He was thought to be used for questioning about the setup. He gets thrown in the warehouse and gets beaten up by them. He eventually gets tied up in a chair, and denies that he knows anything about the setup. He says how he won't tell them anything if he gets let go, only to get punched by Mr. Pink.")
+character8.project = project1
+file8 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382304/Reservoir%20Dogs%20Characters/Officer_wazjnt.png')
+character1.photo.attach(io: file8, filename: "Nash.jpg", content_type: 'image/jpg')
+character8.save!
 
-# character9 = Character.new(first_name: "Inspector", last_name: "Holdaway", birthdate: Time.now, genre: "Male", description: "He is a cop, who teaches Freddy Newendyke aka Mr. Orange how to go undercover and infiltrate Joe Cabot's organization.")
-# character9.project = project1
-# character9.photo.key = "Holdaway_zgrjq0"
-# character9.save!
+character9 = Character.new(first_name: "Inspector", last_name: "Holdaway", birthdate: Time.now, genre: "Male", description: "He is a cop, who teaches Freddy Newendyke aka Mr. Orange how to go undercover and infiltrate Joe Cabot's organization.")
+character9.project = project1
+file9 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382305/Reservoir%20Dogs%20Characters/Holdaway_zgrjq0.png')
+character1.photo.attach(io: file9, filename: "Holdaway.jpg", content_type: 'image/jpg')
+character9.save!
 
-# character10 = Character.new(first_name: "Mr.", last_name: "Blue", birthdate: Time.now, genre: "Male", description: "He is one of the six robbers in a diamond heist that goes wrong. He smokes cigars, and his death is unknown until the end of the film, where Joe Cabot says that he was killed by the cops.")
-# character10.project = project1
-# character10.photo.key = "Mr._Blue_vrs8cd"
-# character10.save!
+character10 = Character.new(first_name: "Mr.", last_name: "Blue", birthdate: Time.now, genre: "Male", description: "He is one of the six robbers in a diamond heist that goes wrong. He smokes cigars, and his death is unknown until the end of the film, where Joe Cabot says that he was killed by the cops.")
+character10.project = project1
+file10 = file = URI.open('https://res.cloudinary.com/diewz4u97/image/upload/v1575382305/Reservoir%20Dogs%20Characters/Mr._Blue_vrs8cd.jpg')
+character1.photo.attach(io: file10, filename: "Blue.jpg", content_type: 'image/jpg')
+character10.save!
 
 ################################### SCENES #####################################
 
@@ -110,15 +119,15 @@ scene4.save!
 
 #################################### MOOD ######################################
 
-mood1 = Mood.new()
-mood1.project = project1
-mood1.save!
+# mood1 = Mood.new()
+# mood1.project = project1
+# mood1.save!
 
-file = URI.open('https://meme.eq8.eu/noidea.jpg')
-mooditem1 = Mooditem.new(left: 0, top: 0)
-mooditem1.photo.attach(io: file, filename: 'some-image.jpg', content_type: 'image/jpg')
-mooditem1.mood = mood1
-mooditem1.save!
+# file = URI.open('https://meme.eq8.eu/noidea.jpg')
+# mooditem1 = Mooditem.new(left: 0, top: 0)
+# mooditem1.photo.attach(io: file, filename: 'some-image.jpg', content_type: 'image/jpg')
+# mooditem1.mood = mood1
+# mooditem1.save!
 
 ################################################################ USER 2 ################################################################
 
