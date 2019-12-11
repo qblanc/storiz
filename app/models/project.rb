1dca6c74459c0831adeb838a2d4621849bc5978e
Quentin Blanc <qblanc@hotmail.com>
2019-12-11 13:34:27 +0100

    project dependecies added in model to enable destroy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :characters
-  has_many :scenes
-  has_one :mood
+  has_many :characters, dependent: :destroy
+  has_many :scenes, dependent: :destroy
+  has_one :mood, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 1 }
   validates :category, presence: true, inclusion: { in: %w(Book Movie "TV Show") } # !!! a implémenter les bitches
